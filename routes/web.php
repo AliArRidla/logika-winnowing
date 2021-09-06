bfe0d1f41ef9a81eed1fd37b37b4aafe10f3d43e
AliArRidla <beeunik0@gmail.com>
2021-09-06 19:40:32 +0700

    convert success

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Route;
+use Smalot\PdfParser\Parser;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [AppController::class,'index']);
+Route::get('/', [AppController::class,'textGet']);
+Route::post('/', [AppController::class,'text']);
+Route::get('/pdf', [AppController::class,'index']);
+Route::post('/pdf', [AppController::class,'pdf']);
+
